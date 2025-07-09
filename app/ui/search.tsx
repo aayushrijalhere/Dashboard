5ff035b597584a446142a495903429a28d86a353
Aayush Rijal <aayushrijal313@gmail.com>
2025-07-09 14:38:56 +0545

    updated app

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,21 +1,24 @@
 'use client';
-
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search(placeholder: string) {
-   const searchParams = useSearchParams();
-const pathname = usePathname();
+export default function Search({placeholder}: {placeholder: string}) {
+
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
-  if(term){
-params.set('query',term);
-  }else{
-    params.delete('query');
-  }
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
 
