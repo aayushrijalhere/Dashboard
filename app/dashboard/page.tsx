5cff773135e40252e635269c2e38beb3b01dcf7c
Aayush Rijal <aayushrijal313@gmail.com>
2025-07-09 14:38:05 +0545

    action and create are added

--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <p>Hello I am Dashboard Page </p>;
-}
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -112,6 +112,5 @@ export async function GET() {
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+      }
 }
